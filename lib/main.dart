4c15bbed4d6fd33dbf01abd4e60a03d6f95151f8
Faisal Hussain Alanazi <58009426+faisaldeveloped@users.noreply.github.com>
2022-10-08 20:12:29 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Food App',
+      title: 'Game App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
